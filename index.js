f21037bdaf7256a650ab8530246b4ab2ccc6ce6e
naderboutros75 <132116377+naderboutros75@users.noreply.github.com>
2023-06-01 00:24:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 var numOfDrumButtoms = document.querySelectorAll(".drum").length;
+var drumsButtons = document.querySelectorAll(".drum");
+
+var tom1 = new Audio("./sounds/tom-1.mp3");
+var tom2 = new Audio("./sounds/tom-2.mp3");
+var tom3 = new Audio("./sounds/tom-3.mp3");
+var tom4 = new Audio("./sounds/tom-4.mp3");
+var snare = new Audio("./sounds/snare.mp3");
+var crash = new Audio("./sounds/crash.mp3");
+var kick = new Audio("./sounds/kick-bass.mp3");
 
 
 for (let i = 0; i < numOfDrumButtoms; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    drumsButtons[i].addEventListener("click", function () {
         switch (this.innerHTML) {
             case "w":
-                var tom1 = new Audio("./sounds/tom-1.mp3");
                 tom1.play();
                 break;
 
             case "a":
-                var tom2 = new Audio("./sounds/tom-2.mp3");
                 tom2.play();
                 break;
 
             case "s":
-                var tom3 = new Audio("./sounds/tom-3.mp3");
                 tom3.play();
                 break;
 
-            case "d":
-                var tom4 = new Audio("./sounds/tom-4.mp3");
+            case "d":                
                 tom4.play();
                 break;
 
-            case "j":
-                var snare = new Audio("./sounds/snare.mp3");
+            case "j":                
                 snare.play();
                 break;
 
-            case "k":
-                var crash = new Audio("./sounds/crash.mp3");
+            case "k":                
                 crash.play();
                 break;
 
-            case "l":
-                var kick = new Audio("./sounds/kick-bass.mp3");
+            case "l":                
                 kick.play();
                 break;
 
-            default: console.log();
+            default:
+                console.log();
                 break;
         }
     });
-
 }
